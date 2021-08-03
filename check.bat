569bd2fa74bd6ee45e3c320ab8a976e8a2dba807
asharoo1 <84122970+asharoo1@users.noreply.github.com>
2021-08-03 18:49:33 +0530

    Update check.bat

--- check.bat
+++ check.bat
@@ -1,10 +1,13 @@
 @echo off
-set /p pemFileName=Enter .pem file Name: 
+
 set /p pemFilePath=Enter .pem file path: 
-set /p zipFilePath=Enter eth2.0-deposit-cli.zip file path:
-set /p instanceCredential=Enter AWS Instance Credntial: 
+set /p zipFilePath=Enter Validator Key zip file path: 
+set /p instanceCredential=Enter AWS public IPv4 and external DNS hostname[eg: ec2-user@ec2-3-87-129-138.compute-1.amazonaws.com]: 
 
+ICACLS "%pemFilePath%" /grant:r "users:(R)" /C
 
-scp -i %pemFilePath% -r %zipFilePath% %instanceCredential%:/home/ec2-user
+scp -i "%pemFilePath%" -r "%zipFilePath%" %instanceCredential%:/home/ec2-user
 
-ssh -i %pemFileName% %instanceCredential%
+@REM ICACLS "%pemFilePath%" /grant:r "users:(R)" /C
+
+ssh -i "%pemFilePath%" %instanceCredential%
