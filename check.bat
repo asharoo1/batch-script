7310a97a0155cccb043f9771d073bd7fa4da3576
asharoo1 <84122970+asharoo1@users.noreply.github.com>
2021-08-05 09:18:15 +0530

    Update check.bat

--- check.bat
+++ check.bat
@@ -4,10 +4,6 @@ set /p pemFilePath=Enter .pem file path:
 set /p zipFilePath=Enter Validator Key zip file path: 
 set /p instanceCredential=Enter AWS public IPv4 and external DNS hostname[eg: ec2-user@ec2-3-87-129-138.compute-1.amazonaws.com]: 
 
-ICACLS "%pemFilePath%" /grant:r "users:(R)" /C
+scp -i %pemFilePath% -r %zipFilePath% %instanceCredential%:/home/ec2-user
 
-scp -i "%pemFilePath%" -r "%zipFilePath%" %instanceCredential%:/home/ec2-user
-
-@REM ICACLS "%pemFilePath%" /grant:r "users:(R)" /C
-
-ssh -i "%pemFilePath%" %instanceCredential%
+ssh -i %pemFilePath% %instanceCredential%
